c9a630155c21a66d00b2c090d01596d6c89beadb
Reşat Onur Kurt <resatonurkurt@gmail.com>
2026-04-20 08:52:10 +0300

    Revert "login"
    This reverts commit 2a00b1ecaae662cb6725ff442d4934de7b78ae29.

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"],
   },
-  webpack: (config, { dev }) => {
-    if (!dev && process.env.REDUCE_WEBPACK_MEMORY === "1") {
-      config.parallelism = 1;
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
