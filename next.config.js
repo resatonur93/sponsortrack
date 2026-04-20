2a00b1ecaae662cb6725ff442d4934de7b78ae29
Reşat Onur Kurt <resatonurkurt@gmail.com>
2026-04-20 03:03:20 +0300

    login

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"],
   },
+  webpack: (config, { dev }) => {
+    if (!dev && process.env.REDUCE_WEBPACK_MEMORY === "1") {
+      config.parallelism = 1;
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
